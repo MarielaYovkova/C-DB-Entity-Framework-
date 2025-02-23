1bfe262ef9dd71c65a788b0712b129bff205ce21
Mariela Yovkova <122797627+MarielaYovkova@users.noreply.github.com>
2025-02-23 15:19:33 +0200

    Add files via upload

---
+++ 01.EntityFrameworkIntroduction/Problem-04.cs
@@ -0,0 +1,37 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            string result = GetEmployeesWithSalaryOver50000(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                });
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} - {e.Salary:F2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
