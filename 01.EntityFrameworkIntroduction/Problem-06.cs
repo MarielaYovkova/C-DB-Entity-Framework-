451310b14d3f4549bb4bc114654e56f3677fd19b
Mariela Yovkova <122797627+MarielaYovkova@users.noreply.github.com>
2025-02-24 20:34:50 +0200

    Add files via upload

---
+++ 01.EntityFrameworkIntroduction/Problem-06.cs
@@ -0,0 +1,44 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            string result = AddNewAddressToEmployee(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            const string newAddressText = "Vitoshka 15";
+            const int newAddressTownId = 4;
+
+            Address newAddress = new Address()
+            {
+                AddressText = newAddressText,
+                TownId = newAddressTownId
+            };
+
+            Employee nakovEmployee = context.Employees
+                .FirstOrDefault(e => e.LastName == "Nakov")!;
+
+            nakovEmployee.Address = newAddress;
+
+            context.SaveChanges();
+
+            string[] addresses = context.Employees
+                .Where(e => e.AddressId.HasValue)
+                .OrderByDescending(e => e.AddressId)
+                .Select(e => e.Address!.AddressText)
+                .Take(10)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, addresses);
+        }
+    }
+}
