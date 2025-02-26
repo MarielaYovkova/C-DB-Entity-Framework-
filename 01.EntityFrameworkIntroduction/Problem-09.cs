205285cb48e0dbc021e024780e6954ed99922f61
Mariela Yovkova <122797627+MarielaYovkova@users.noreply.github.com>
2025-02-26 17:22:42 +0200

    Add files via upload

---
+++ 01.EntityFrameworkIntroduction/Problem-09.cs
@@ -0,0 +1,43 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            string result = GetEmployee147(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployee147(SoftUniContext context)
+        {
+            var employee147 = context.Employees
+                .Where(e => e.EmployeeId == 147)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    Projects = e.EmployeesProjects
+                        .Select(ep => ep.Project.Name)
+                        .OrderBy(p => p)
+                        .ToList()
+                })
+                .FirstOrDefault()!;
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{employee147.FirstName} {employee147.LastName} - {employee147.JobTitle}");
+            foreach (var project in employee147.Projects)
+            {
+                sb.AppendLine(project);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
