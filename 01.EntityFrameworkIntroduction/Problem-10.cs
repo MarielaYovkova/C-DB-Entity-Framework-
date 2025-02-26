a5a6ddbe30d4f5fc532176ba7feab901e11472c5
Mariela Yovkova <122797627+MarielaYovkova@users.noreply.github.com>
2025-02-26 17:22:59 +0200

    Add files via upload

---
+++ 01.EntityFrameworkIntroduction/Problem-10.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            string result = GetDepartmentsWithMoreThan5Employees(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var departments = context.Departments
+                .Include(d => d.Employees)
+                .Where(d => d.Employees.Count > 5)
+                .OrderBy(d => d.Employees.Count)
+                .ThenBy(d => d.Name)
+                .ToArray();
+
+            foreach (var d in departments)
+            {
+                sb.AppendLine($"{d.Name} - {d.Manager.FirstName} {d.Manager.LastName}");
+
+                var employees = d.Employees
+                    .OrderBy(e => e.FirstName)
+                    .ThenBy(e => e.LastName)
+                    .ToList();
+
+                foreach (var e in employees)
+                {
+                    sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
