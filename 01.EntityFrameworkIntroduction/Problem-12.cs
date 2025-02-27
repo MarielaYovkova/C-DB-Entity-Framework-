bc798312070c960fef1b93da4bd890c5aba2038f
Mariela Yovkova <122797627+MarielaYovkova@users.noreply.github.com>
2025-02-27 19:35:52 +0200

    Add files via upload

---
+++ 01.EntityFrameworkIntroduction/Problem-12.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            string result = IncreaseSalaries(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string IncreaseSalaries(SoftUniContext context)
+        {
+            List<string> departments = new List<string>() { "Engineering", "Tool Design", "Marketing", "Information Services" };
+
+            var employees = context.Employees
+                .Where(e => departments.Any(d => d == e.Department.Name))
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName);
+
+            foreach (var employee in employees)
+            {
+                employee.Salary *= 1.12M;
+            }
+
+            context.SaveChanges();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var e in employees.ToList())
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} (${e.Salary:F2})");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
