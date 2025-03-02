c1aaca10f3678fc3707913f5962bfc568c0b83d1
Mariela Yovkova <122797627+MarielaYovkova@users.noreply.github.com>
2025-03-02 16:28:21 +0200

    Add files via upload

---
+++ 01.EntityFrameworkIntroduction/Problem-13.cs
@@ -0,0 +1,41 @@
+﻿using SoftUni.Data;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            string result = GetEmployeesByFirstNameStartingWithSa(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string GetEmployeesByFirstNameStartingWithSa(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(e => e.FirstName.StartsWith("Sa"))
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    e.Salary
+                })
+                .OrderBy(e => e.FirstName)
+                .ThenBy(e => e.LastName)
+                .ToList();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} - {e.JobTitle} - (${e.Salary:F2})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 01.EntityFrameworkIntroduction/Problem-14.cs
@@ -0,0 +1,42 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniContext dbContext = new SoftUniContext();
+            string result = DeleteProjectById(dbContext);
+
+            Console.WriteLine(result);
+        }
+
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            const int deleteProjectId = 2;
+
+            var employeeProjectsDelete = context.EmployeesProjects
+                .Where(ep => ep.ProjectId == deleteProjectId)
+                .ToList();
+
+            context.EmployeesProjects.RemoveRange(employeeProjectsDelete);
+
+            var deleteProject = context.Projects.Find(deleteProjectId);
+
+            if (deleteProject != null)
+            {
+                context.Projects.Remove(deleteProject);
+            }
+
+            context.SaveChanges();
+
+            string[] projectNames = context.Projects
+                .Select(p => p.Name)
+                .Take(10)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, projectNames);
+        }
+    }
+}
